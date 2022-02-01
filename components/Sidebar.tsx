0eba35c17c5d8d54e8532e6aaf49bdcacc90febe
Alex Rueb <rueba@oregonstate.edu>
2022-01-31 21:07:19 -0600

    added playlists to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import {
   HomeIcon,
   SearchIcon,
@@ -7,12 +8,23 @@ import {
   RssIcon,
   LogoutIcon,
 } from '@heroicons/react/outline'
-
 import { signOut, useSession } from 'next-auth/react'
+import useSpotify from '../hooks/useSpotify'
 
 export default function Sidebar() {
+  const spotifyApi = useSpotify()
   const { data: session, status } = useSession()
+  const [playlists, setPlaylists] = useState([])
 
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data: any) => {
+        setPlaylists(data.body.items)
+      })
+    }
+  }, [session, spotifyApi])
+
+  console.log('playlists:', playlists)
   console.log('session:', session)
   return (
     <div className="h-screen overflow-y-scroll border-r border-gray-900 p-5 text-sm text-gray-500 scrollbar-hide">
@@ -53,7 +65,9 @@ export default function Sidebar() {
         <hr className="border-t-[0.1px] border-gray-900" />
 
         {/* playlists */}
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        {playlists.map((playlist: any) => {
+          return <p key={playlist.id}>{playlist.name}</p>
+        })}
       </div>
     </div>
   )
